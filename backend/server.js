2078f8b39744ee502a2a6a35b29b664ca01dc68a
rizwankhan <super047240@gmail.com>
2025-05-03 17:34:22 +0530

    this is cult porject

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const connectDB = require('./config/db')
+require('dotenv').config()
+// const TodoRoute = require('./routes/todos')
+const authRouter = require('./routes/atuhroute')
+const cors = require('cors')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use('/api/v1/auth', authRouter)
+// app.use('/api/v1/todo', TodoRoute)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
+}
+
+// default route
+app.get('/', (req, res) => {
+  res.send(`<h1>this is homepage baby</h1>`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`)
+  connectDB()
+})
